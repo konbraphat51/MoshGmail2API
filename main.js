4d4f331c487dd9e05badb86b2e0c1a5ae1d16981
konbraphat51 <konbraphat@gmail.com>
2024-03-09 12:26:14 +0900

    get email content

--- main.js
+++ main.js
@@ -4,3 +4,17 @@
 const now = Math.floor(new Date().getTime() / 1000)
 const start = now - 60 * 60 * 24 // 24時間前から
 const threads = GmailApp.search(`after:${start}`)
+
+//メールの内容を取り出し
+var emails = []
+threads.forEach((thread) => {
+	thread.getMessages().forEach((message) => {
+		const email = {
+			body: message.getPlainBody(),
+			date: message.getDate(),
+			from: message.getFrom(),
+			subject: message.getSubject(),
+		}
+		emails.push(email)
+	})
+})
