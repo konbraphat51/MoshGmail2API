e77882a2599d87253f875c684ce1821e249dcc1d
konbraphat51 <konbraphat@gmail.com>
2024-03-09 12:31:04 +0900

    only mosh registered email

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 //GASスクリプト
 
+//>>定数類
+//Mosh側仕様変更に注意
+const moshEmail = "hello@themosh.jp"
+const registredTitle = "サービスへのお申し込みがありました"
+//<<
+
 //メール取り出し
 const now = Math.floor(new Date().getTime() / 1000)
 const start = now - 60 * 60 * 1 // 1時間前から
 const threads = GmailApp.search(`after:${start}`)
 
-//メールの内容を取り出し
+//Moshのサービス申し込みメールのみ抽出
 var emails = []
 threads.forEach((thread) => {
 	thread.getMessages().forEach((message) => {
@@ -15,6 +21,19 @@ threads.forEach((thread) => {
 			from: message.getFrom(),
 			subject: message.getSubject(),
 		}
+
+		//送信元がMoshでないなら...
+		if (email.from.indexOf(moshEmail) === -1) {
+			//...スキップ
+			return
+		}
+
+		//申し込みメールでないなら...
+		if (email.subject.indexOf(registredTitle) === -1) {
+			//...スキップ
+			return
+		}
+
 		emails.push(email)
 	})
 })
